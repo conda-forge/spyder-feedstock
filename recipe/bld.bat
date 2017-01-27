6c3356fea5e11511ea1a7fa2f9e79e16a6dd5179
Morten Enemark Lund <melund@gmail.com>
2017-01-27 21:48:33 +0100

    Port changes from 3.0.2 recipe on anaconda

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-python setup.py install
+%PYTHON% setup.py install
 if errorlevel 1 exit 1
 
 set MENU_DIR=%PREFIX%\Menu
@@ -6,10 +6,11 @@ IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
 copy %SRC_DIR%\img_src\spyder.ico %MENU_DIR%\
 if errorlevel 1 exit 1
-copy %SRC_DIR%\img_src\spyder_light.ico %MENU_DIR%\
-if errorlevel 1 exit 1
+REM copy %SRC_DIR%\img_src\spyder_light.ico %MENU_DIR%\
+REM if errorlevel 1 exit 1
 copy %RECIPE_DIR%\menu-windows.json %MENU_DIR%\spyder_shortcut.json
 if errorlevel 1 exit 1
 
 del %SCRIPTS%\spyder_win_post_install.py
 del %SCRIPTS%\spyder.bat
+del %SCRIPTS%\spyder
