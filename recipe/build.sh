12c672df493289d275b916d222ab5ffa0cf8592a
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2023-12-21 10:50:13 -0800

    Update build.sh and bld.bat
    macOS arm64 uses GNU instead of BSD, however there is no need to make edits in-place ("-i '' -e" vs "-i -e"), and therefore no difference between GNU and BSD in this implementation.

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,3 +5,16 @@ $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 rm -rf $PREFIX/man
 rm -f $PREFIX/bin/spyder_win_post_install.py
 rm -rf $SP_DIR/Sphinx-*
+
+# Create the Menu directory
+mkdir -p "${PREFIX}/Menu"
+
+# Copy menu.json template, replacing version
+sed -e "s/__PKG_VERSION__/${PKG_VERSION}/g" "${RECIPE_DIR}/spyder-menu.json" > "${PREFIX}/Menu/spyder-menu.json"
+
+# Copy application icons
+if [[ $OSTYPE == "darwin"* ]]; then
+    cp "${RECIPE_DIR}/spyder.icns" "${PREFIX}/Menu/spyder.icns"
+else
+    cp "${RECIPE_DIR}/spyder.png" "${PREFIX}/Menu/spyder.png"
+fi
