6bcd175ca3bddffc568f27054e4723c790cbfce8
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-01-12 15:00:12 -0800

    Fix typo

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -7,7 +7,7 @@ if [[ -f "${PREFIX}/Menu/conda-based-app" ]]; then
     # Installed in installer environment, abridge shortcut name
     menu="${PREFIX}/Menu/spyder-menu.json"
     sed ${opts[@]} "s/ \(\{\{ ENV_NAME \}\}\)//g" $menu
-elif [[ -d "${PREFIX}/condabin" & -d "${PREFIX}/envs" ]]; then
+elif [[ -d "${PREFIX}/condabin" && -d "${PREFIX}/envs" ]]; then
     # Installed in a base environment, use distribution name
     sed ${opts[@]} "s/ENV_NAME/DISTRIBUTION_NAME/g" $menu
 fi
