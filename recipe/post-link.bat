c8694fc6f0ecd84195821ad591be31d5696539fe
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-03-09 14:02:02 -0800

    Continue to use v2 shortcut if CONDA_PYTHON_EXE does not exist.
    V1 shortcuts do not work with without this environment variable (e.g. Micromamba), since a base environment is not known. The shortcut is created, but is broken. It is better to not create a shortcut.
    
    If there is an error, or v2 shortcut is broken, then we will deal with that issue when it arises.

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -3,25 +3,40 @@ setlocal ENABLEDELAYEDEXPANSION
 
 set menudir=%PREFIX%\Menu
 set menu=%menudir%\spyder-menu.json
+set logfile=%PREFIX%\.messages.txt
 
-call :conda_based
+rem  Check for conda-based install
+if exist "%menudir%\conda-based-app" (
+    rem  Abridge shortcut name
+    call :patch " ^({{ ENV_NAME }}^)="
 
-call :menuinst_ver || goto :exit
+    rem  Nothing more to do for conda-based install
+    goto :exit
+)
 
-call :base_env
+rem  Check for CONDA_PYTHON_EXE
+if not exist "%conda_python_exe%" (
+    rem  CONDA_PYTHON_EXE environment variable does not exist.
+    rem  v1 type shortcuts will not work
+    goto :base_env
+)
+
+rem  Check menuinst version
+for /F "tokens=*" %%i in (
+    '%conda_python_exe% -c "from importlib.metadata import version; from packaging.version import parse; print(parse(version('menuinst')) < parse('2'))"'
+) do (
+    if "%%~i"=="True" call :use_menu_v1
+)
+
+:base_env
+    if exist "%PREFIX%\condabin\" if exist "%PREFIX%\envs\" (
+        rem  Installed in a base environment, use distribution name
+        call :patch "ENV_NAME=DISTRIBUTION_NAME"
+    )
+    goto :exit
 
 :exit
     exit /b %errorlevel%
-
-:conda_based
-    if exist "%menudir%\conda-based-app" (
-        :: Installed in installer environment, abridge shortcut name
-        call :patch " ^({{ ENV_NAME }}^)="
-
-        :: Nothing more to do for conda-based install
-        goto :exit
-    )
-    goto :eof
 
 :patch
     set tmpmenu=%menudir%\spyder-menu-tmp.json
@@ -33,26 +48,11 @@ call :base_env
     move /y "%tmpmenu%" "%menu%"
     goto :eof
 
-:menuinst_ver
-    :: How to robustly call the base environment python?
-    for /F "tokens=*" %%i in (
-        '%conda_python_exe% -c "from importlib.metadata import version; from packaging.version import parse; print(parse(version('menuinst')) < parse('2'))"'
-    ) do (
-        set isv1=%%~i
-    )
-    if "%isv1%"=="True" (
-        copy /y "%menudir%\spyder-menu-v1.json.bak" "%menu%"
+:use_menu_v1
+    copy /y "%menudir%\spyder-menu-v1.json.bak" "%menu%"
 
-        :: Notify user
-        echo. >> %PREFIX%\.messages.txt
-        echo Warning! Using menuinst v1.>> %PREFIX%\.messages.txt
-        echo Please update to menuinst v2 in the base environment and reinstall Spyder>> %PREFIX%\.messages.txt
-    )
-    goto :eof
-
-:base_env
-    if exist "%PREFIX%\condabin\" if exist "%PREFIX%\envs\" (
-        :: Installed in a base environment, use distribution name
-        call :patch "ENV_NAME=DISTRIBUTION_NAME"
-    )
+    rem  Notify user
+    echo. >> %logfile%
+    echo Warning: Using menuinst v1 >> %logfile%
+    echo Please update to menuinst v2 in the base environment and reinstall Spyder >> %logfile%
     goto :eof
