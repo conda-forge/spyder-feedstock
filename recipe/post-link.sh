99370767a8997098b10580014d963de0aaaa5a30
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-05-02 18:33:30 -0700

    Fix sed once and for all. Array options for script and cat-ing to script are incompatible: ("-i" "''" "-e") works for cat-ing but not for script; ("-i" '' "-e") works for script but not cat-ing. For situations that require both script and cat-ing that must work for both GNU and BSD, the only solution is to use "-i.bak -e" and "rm *.bak"

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-[[ $(sed --version 2>/dev/null) ]] && opts=("-i" "-E") || opts=("-i" "''" "-E")
+[[ $(sed --version 2>/dev/null) ]] && opts=("-i" "-E") || opts=("-i" "" "-E")
 menu="${PREFIX}/Menu/spyder-menu.json"
 
 if [[ -f "${PREFIX}/Menu/conda-based-app" ]]; then
     # Installed in installer environment, abridge shortcut name
-    sed ${opts[@]} "s/ \(\{\{ ENV_NAME \}\}\)//g" $menu
+    sed "${opts[@]}" "s/ \(\{\{ ENV_NAME \}\}\)//g" $menu
 elif [[ -d "${PREFIX}/condabin" && -d "${PREFIX}/envs" ]]; then
     # Installed in a base environment, use distribution name
-    sed ${opts[@]} "s/ENV_NAME/DISTRIBUTION_NAME/g" $menu
+    sed "${opts[@]}" "s/ENV_NAME/DISTRIBUTION_NAME/g" $menu
 fi
