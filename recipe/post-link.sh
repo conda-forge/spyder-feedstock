c92b6084a495f1988da30f564e07770ddf17a010
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-06-27 11:26:25 -0700

    Menuinst >2.1 includes option for base/non-base environment names

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -7,7 +7,4 @@ menu="${PREFIX}/Menu/spyder-menu.json"
 if [[ -f "${PREFIX}/Menu/conda-based-app" ]]; then
     # Installed in installer environment, abridge shortcut name
     sed "${opts[@]}" "s/ \(\{\{ ENV_NAME \}\}\)//g" $menu
-elif [[ -d "${PREFIX}/condabin" && -d "${PREFIX}/envs" ]]; then
-    # Installed in a base environment, use distribution name
-    sed "${opts[@]}" "s/ENV_NAME/DISTRIBUTION_NAME/g" $menu
 fi
