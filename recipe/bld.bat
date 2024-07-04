af6d6a0c9f230bef89881438958b32da18b725e4
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-07-04 10:50:04 -0700

    Create subroutine for replacing variables in menu file

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,22 +6,21 @@ if errorlevel 1 exit 1
 set MENU_DIR=%PREFIX%\Menu
 IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
+rem  Copy Spyder's icon
 copy %RECIPE_DIR%\spyder.ico %MENU_DIR%\spyder.ico
 
+rem  Replace variables in menu files
 for /F "delims=. tokens=1" %%i in ("%PKG_VERSION%") do set PKG_MAJOR_VER=%%i
+call :replace spyder-menu.json spyder-menu.json
+call :replace spyder-menu-v1.json spyder-menu-v1.json.bak
 
-for /f "delims=" %%i in (%RECIPE_DIR%\spyder-menu.json) do (
-    set s=%%i
-    set s=!s:__PKG_VERSION__=%PKG_VERSION%!
-    echo !s:__PKG_MAJOR_VER__=%PKG_MAJOR_VER%!>> %MENU_DIR%\spyder-menu.json
-)
+:exit
+    exit /b %errorlevel%
 
-for /f "delims=" %%i in (%RECIPE_DIR%\spyder-menu-v1.json) do (
-    set s=%%i
-    set s=!s:__PKG_VERSION__=%PKG_VERSION%!
-    echo !s:__PKG_MAJOR_VER__=%PKG_MAJOR_VER%!>> %MENU_DIR%\spyder-menu-v1.json.bak
-)
-
-del %SCRIPTS%\spyder_win_post_install.py
-del %SCRIPTS%\spyder.bat
-del %SCRIPTS%\spyder
+:replace
+    for /f "delims=" %%i in (%RECIPE_DIR%\%1) do (
+        set s=%%i
+        set s=!s:__PKG_VERSION__=%PKG_VERSION%!
+        echo !s:__PKG_MAJOR_VER__=%PKG_MAJOR_VER%!>> %MENU_DIR%\%2
+    )
+    goto :eof
