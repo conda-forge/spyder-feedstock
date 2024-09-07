fd302660c8dd2a0217a8960acb553a953766c439
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-09-07 13:03:30 -0700

    Change app_user_model_id, StartupWMClass, and CFBundleIdentifier to include Spyder major version and (possibly) environment name. Note CFBundleIdentifier cannot contain underscores.

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -7,10 +7,14 @@ menu="${PREFIX}/Menu/spyder-menu.json"
 if [[ -f "${PREFIX}/Menu/conda-based-app" ]]; then
     # Installed in installer environment, abridge shortcut name
     sed "${opts[@]}" "s/ \(\{\{ ENV_NAME \}\}\)//g" $menu
+    sed "${opts[@]}" "s/-__CFBID_ENV__//g" $menu
 
     # Nothing more to do for conda-based-installers
     exit
 fi
+
+env_name=$(basename ${PREFIX//_/-})
+sed "${opts[@]}" "s/__CFBID_ENV__/${env_name}/g" $menu
 
 # Do not create shortcut for menuinst version <2.1.2
 menuinst_version=$($CONDA_PYTHON_EXE -c "import menuinst; print(menuinst.__version__)" 2>/dev/null || echo "0.0.0")
