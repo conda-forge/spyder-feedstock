416dbf102145db1ba8ada8600e0daa289f2147af
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-09-08 23:05:04 -0700

    Check menuinst version and take appropriate action.

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -8,3 +8,11 @@ if [[ -f "${PREFIX}/Menu/conda-based-app" ]]; then
     # Installed in installer environment, abridge shortcut name
     sed "${opts[@]}" "s/ \(\{\{ ENV_NAME \}\}\)//g" $menu
 fi
+
+# Do not create shortcut for menuinst version <2.1.2
+menuinst_version=$($CONDA_PYTHON_EXE -c "import menuinst; print(menuinst.__version__)")
+if [[ "$menuinst_version" < "2.1.2" ]]; then
+    mv -f ${menu} ${menu}.bak
+    echo "Warning: Spyder shortcut will not be created." >> ${PREFIX}/.message.txt
+    echo "Please update to menuinst >=2.1.2 in the base environment and reinstall Spyder." >> ${PREFIX}/.message.txt
+fi
