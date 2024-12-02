ecff64fb40dad3c0519474054229973076a18d34
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2024-12-02 07:46:03 -0800

    Create spyder-base package that does not depend on pyqt. Use jinja variable for build number and bump. Remove reference to python version in tests to ensure that spyder package has same hash regardless of runner.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
 setlocal ENABLEDELAYEDEXPANSION
 
+set SPYDER_QT_BINDING=conda-forge
 %PYTHON% -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export SPYDER_QT_BINDING=conda-forge
 $PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
 
 rm -rf $PREFIX/man
