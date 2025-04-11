35964e73515a7270ae8102b690388930d4927350
Eric Larson <larson.eric.d@gmail.com>
2025-04-11 10:56:03 -0400

    FIX: Push it

---
+++ recipe/test_gui.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+TO=20s
+
+# Launch it
+if [[ "$(uname -s)" == *'Linux'* ]]; then
+    export EXTRA=xvfb-run
+else
+    export EXTRA=
+fi
+set +x
+which spyder
+$EXTRA timeout $TO spyder
+set -x
+RESULT=$?
+if [[ $RESULT -eq 124 ]]; then
+    echo "Spyder succeeded with timeout"
+else
+    echo "Spyder failed with error code $RESULT (should be 124 for timeout)"
+    exit 1
+fi
