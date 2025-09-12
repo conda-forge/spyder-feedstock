bd01e4616815af3513bb3d15ff4563f2515344ef
dalthviz <16781833+dalthviz@users.noreply.github.com>
2025-09-11 20:14:36 -0500

    Update to 6.1.0b1

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -12,38 +12,56 @@ del %scriptsdir%\spyder-script.py
 
 rem  Check for conda-based install
 if exist "%menudir%\conda-based-app" (
-    rem  Abridge shortcut name
-    call :patch " ^({{ ENV_NAME }}^)=" %menu%
-
+    call :conda_based_install
     rem  Nothing more to do for conda-based install
     goto :exit
 )
 
-rem  Check for CONDA_PYTHON_EXE
-if not exist "%conda_python_exe%" (
-    rem  CONDA_PYTHON_EXE environment variable does not exist.
-    rem  v1 type shortcuts will not work
-    goto :exit
-)
-
-rem  Check menuinst version
-for /F "tokens=*" %%i in (
-    '%conda_python_exe% -c "import menuinst; print(menuinst.__version__)"'
-) do (
-    if "%%~i" lss "2.1.2" call :use_menu_v1
-)
+call :not_conda_based_install
 
 :exit
     exit /b %errorlevel%
 
+:conda_based_install
+    rem  Abridge shortcut name
+    call :patch " ^({{ ENV_NAME }}^)=" %menu%
+
+    rem  Prevent using user site-packages
+    rem  See https://github.com/spyder-ide/spyder/issues/24773
+    set site=%PREFIX%\envs\spyder-runtime\Lib\site.py
+    set site_tmp=%PREFIX%\envs\spyder-runtime\Lib\site.py.bak
+    (for /f "delims=" %%i in ('type "%site%" ^| findstr /n "^"') do (
+        set "s=%%i"
+        set "s=!s:*:=!"
+        if "!s!"=="ENABLE_USER_SITE = None" set s=!s:None=False!
+        echo.!s!
+    )) > %site_tmp% && move /y %site_tmp% %site% >nul
+
+    goto :eof
+
+:not_conda_based_install
+    rem  Check for CONDA_PYTHON_EXE
+    if not exist "%conda_python_exe%" (
+        rem  CONDA_PYTHON_EXE environment variable does not exist.
+        rem  v1 type shortcuts will not work
+        goto :exit
+    )
+
+    rem  Check menuinst version
+    for /F "tokens=*" %%i in (
+        '%conda_python_exe% -c "import menuinst; print(menuinst.__version__)"'
+    ) do (
+        if "%%~i" lss "2.1.2" call :use_menu_v1
+    )
+
 :patch
     set tmpmenu=%menudir%\tmp.json
     set findreplace=%~1
-    for /f "delims=" %%a in (%~2) do (
-        set s=%%a
-        echo !s:%findreplace%!>> "%tmpmenu%"
-    )
-    move /y "%tmpmenu%" "%~2"
+    (for /f "delims=" %%a in ('type "%~2" ^| findstr /n "^"') do (
+        set "s=%%a"
+        set "s=!s:*:=!"
+        echo !s:%findreplace%!
+    )) > "%tmpmenu%" && move /y "%tmpmenu%" "%~2" >nul
     goto :eof
 
 :use_menu_v1
