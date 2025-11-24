3f08e48cc5d5f519a9eacfeb90bf13493e779d54
Ryan Clary <9618975+mrclary@users.noreply.github.com>
2025-11-24 14:51:36 -0800

    Only require 2.4.0 for macOS

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -22,9 +22,12 @@ env_name=$(basename ${PREFIX//_/-})
 sed "${opts[@]}" "s/__CFBID_ENV__/${env_name}/g" $menu
 
 # Do not create shortcut for menuinst version less than min version
-menuinst_min_version = "2.4.0"
+menuinst_min_version="2.1.2"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    menuinst_min_version="2.4.0"
+fi
 menuinst_version=$($CONDA_PYTHON_EXE -c "import menuinst; print(menuinst.__version__)" 2>/dev/null || echo "0.0.0")
-if [[ "$menuinst_version" < ${menuinst_min_version} ]]; then
+if [[ "$menuinst_version" < "${menuinst_min_version}" ]]; then
     mv -f ${menu} ${menu}.bak
     echo "Warning: Spyder shortcut will not be created." >> ${PREFIX}/.message.txt
     echo "Please update to menuinst >=${menuinst_min_version} in the base environment and reinstall Spyder." >> ${PREFIX}/.message.txt
