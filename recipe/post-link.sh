38d13a297002f8980de48d52107bb6cc1bec773a
Ryan Clary <mrclary@users.noreply.github.com>
2026-07-27 23:40:09 -0700

    Only test for menuinst version if not conda-based app. CONDA_PYTHON_EXE is not defined in installers. This was causing the shortcuts to not be created and the installer tests to fail.

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -18,27 +18,27 @@ if [[ -f "${PREFIX}/Menu/conda-based-app" ]]; then
 
     # Abridge shortcut name
     sed "${opts[@]}" "s/ \(\{\{ ENV_NAME \}\}\)//g" $menu  # macOS application bundle name
-    sed "${opts[@]}" "s/__CFBID_ENV__//g" $menu  # macOS CFBundleIdentifier
+    sed "${opts[@]}" "s/-__CFBID_ENV__//g" $menu  # macOS CFBundleIdentifier
 
     # Prevent using user site-packages
     # See https://github.com/spyder-ide/spyder/issues/24773
     site=$(find ${PREFIX}/lib/python* -name "site.py")
     sed "${opts[@]}" 's/^ENABLE_USER_SITE = None/ENABLE_USER_SITE = False/g' "${site}"
-fi
+else
+    # Do not create shortcut for menuinst version less than min version
+    menuinst_version=$($CONDA_PYTHON_EXE -c "import menuinst; print(menuinst.__version__)" 2>/dev/null || echo "0.0.0")
+    last_version=$(echo -e "${menuinst_version}\n${menuinst_min_version}" | sort -V | tail -n1)
+    if [[ "${menuinst_version}" != "${last_version}" ]]; then
+        mv -f ${menu} ${menu}.bak
+        echo "Warning: Spyder shortcut will not be created." >> ${PREFIX}/.message.txt
+        echo "Please update to menuinst >=${menuinst_min_version} in the base environment and reinstall Spyder." >> ${PREFIX}/.message.txt
+        exit 0
+    fi
 
-# Do not create shortcut for menuinst version less than min version
-menuinst_version=$($CONDA_PYTHON_EXE -c "import menuinst; print(menuinst.__version__)" 2>/dev/null || echo "0.0.0")
-last_version=$(echo -e "${menuinst_version}\n${menuinst_min_version}" | sort -V | tail -n1)
-if [[ "${menuinst_version}" != "${last_version}" ]]; then
-    mv -f ${menu} ${menu}.bak
-    echo "Warning: Spyder shortcut will not be created." >> ${PREFIX}/.message.txt
-    echo "Please update to menuinst >=${menuinst_min_version} in the base environment and reinstall Spyder." >> ${PREFIX}/.message.txt
-    exit 0
+    # Replace __CFBID_ENV__ because it does not conform to schema for CFBundleIdentifier
+    env_name=$(basename ${PREFIX//_/-})  # CFBundleIdentifier cannot have underscore
+    sed "${opts[@]}" "s/__CFBID_ENV__/${env_name}/g" $menu
 fi
-
-# Replace __CFBID_ENV__ because it does not conform to schema for CFBundleIdentifier
-env_name=$(basename ${PREFIX//_/-})  # CFBundleIdentifier cannot have underscore
-sed "${opts[@]}" "s/__CFBID_ENV__/${env_name}/g" $menu
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Get CFBundleIdentifier
